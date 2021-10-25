c5efd2760e3c460f6f45106d63933c58abf2494f
cwdaggs <cwdaggs@ucdavis.edu>
2021-10-25 11:18:09 -0700

    Working char stream, added .x file

---
+++ parser/src/parser.rs
@@ -0,0 +1,114 @@
+mod scanner;
+use scanner::*;
+
+pub struct Parser {
+    scan: Scanner
+}
+
+//{} for repetition
+//[] for optional
+// () for grouping
+
+impl Parser {
+    // has a while loop calling get next token or whatever from scanner
+    pub fn program() {
+        //if next token = void
+        //main_declaration()
+    }
+
+    fn declaration() {
+
+    }
+
+    fn main_declaration() {
+
+    }
+
+    fn function_definition() {
+
+    }
+
+    fn declaration_type() {
+
+    }
+
+    fn variable_declaration() {
+
+    }
+
+    fn function_declaration() {
+
+    }
+
+    fn block() {
+
+    }
+
+    fn parameter_block() {
+
+    }
+
+    fn data_type() {
+
+    }
+
+    fn constant() {
+
+    }
+
+    fn statement() {
+
+    }
+
+    fn parameter() {
+
+    }
+
+    fn integer_type() {
+
+    }
+    
+    fn float_type() {
+
+    }
+
+    fn assignment() {
+
+    }
+
+    fn while_loop() {
+
+    }
+
+    fn if_statement() {
+
+    }
+
+    fn expression() {
+
+    }
+
+    fn simple_expression() {
+
+    }
+
+    fn term() {
+
+    }
+
+    fn factor() {
+
+    }
+
+    fn relation_operator() {
+
+    }
+
+    fn add_operator() {
+
+    }
+
+    fn mult_operator() {
+
+    }
+}
